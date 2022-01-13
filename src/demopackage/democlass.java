91c89954a5cb6ceefa62ba9afb82d1ca313d1963
Dinesh kumar <dineshkumar6380704237@gmail.com>
2022-01-13 15:46:17 +0530

    firstcommit

---
+++ src/demopackage/democlass.java
@@ -0,0 +1,11 @@
+package demopackage;
+
+public class democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hii this is Dinesh");
+
+	}
+
+}
