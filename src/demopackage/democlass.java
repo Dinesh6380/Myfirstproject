f8f27130ab83c4d0bbe8c3070bf59197c0f3c4ca
Dinesh6380 <71383219+Dinesh6380@users.noreply.github.com>
2022-01-18 17:07:28 +0530

    Update democlass.java

--- src/demopackage/democlass.java
+++ src/demopackage/democlass.java
@@ -5,7 +5,7 @@ public class democlass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hii this is Dinesh");
-
+		System.out.println("let's try this");
 	}
 
 }
